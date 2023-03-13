17bddf2a4dd94e842f4e89f390908ac9aac4d021
milvio123 <happyshah909@gmail.com>
2023-03-13 00:03:31 -0700

    working

--- scraper.py
+++ scraper.py
@@ -1,36 +1,98 @@
 import requests;
+import json;
 import html;
 from bs4 import BeautifulSoup as soup;
 import urllib
 import urllib.request
 import wget
 
+#get total number of pages
 
-url = " https://gitlab.com/wireshark/wireshark/-/tags" #user-input
-page_content = requests.get(url).content
+#get project id
+urluser = " https://gitlab.com/wireshark/wireshark"
+page_content = requests.get(urluser).content
 soup_page = soup(page_content, 'html.parser')
-print(soup_page)
-zip_links_list_nourl = []
-zip_links_list_url= []
+for id in soup_page.findAll('div', {"id": "js-tree-ref-switcher"}):
+    projectid = id.get('data-project-id')
+print(projectid)
 
-links = soup_page.findAll("a",{"class": "gl-button btn btn-sm btn-confirm"})
-# for link in soup_page.find_all('a', {"class": "gl-button btn btn-sm btn-confirm"}):
-#     zip_link = link.get('href')
-#     url_beginning+=zip_link
-#     zip_links_list.append(url_beginning)
-# print(zip_links_list)
 
-for link in soup_page.find_all('a', {"class": "gl-button btn btn-sm btn-confirm"}):
-    zip_link = link.get('href')
-    zip_links_list_nourl.append(zip_link)
+urluser = "https://gitlab.com/wireshark/wireshark"
+urlchangedapi = "https://gitlab.com/api/v4/projects/" + projectid + "/repository/tags/?per_page=20&page=1"
+data = requests.get(urlchangedapi)
+totalpages = data.headers["X-Total-Pages"]
+print(totalpages)
 
-for link in zip_links_list_nourl:
-    url_beginning = "https://gitlab.com"
-    url_beginning+=link
-    zip_links_list_url.append(url_beginning)
+index = 1
+while index <= int(totalpages) :
+
+    pagenumber = index 
+    url = urluser + "/-/tags?page=" + str(pagenumber)
+    page_content = requests.get(url).content
+    soup_page = soup(page_content, 'html.parser')
+    zip_links_list_nourl = []
+    zip_links_list_url= []
+
+    # links = soup_page.findAll("a",{"class": "gl-button btn btn-sm btn-confirm"})
+
+    for link in soup_page.find_all('a', {"class": "gl-button btn btn-sm btn-confirm"}):
+           zip_link = link.get('href')
+           zip_links_list_nourl.append(zip_link)
+
+    for link in zip_links_list_nourl:
+        url_beginning = "https://gitlab.com"
+        url_beginning+=link
+        zip_links_list_url.append(url_beginning)
     
-for i in zip_links_list_url:
-    wget.download(i)
+    for i in zip_links_list_url:
+        wget.download(i)
+    index+=1
+
+
+
+
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
